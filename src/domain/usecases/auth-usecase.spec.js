b7d4aa720c8bccffed074c1830a5008e8577d5bb
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-15 20:58:50 +0000

    ensure AuthUseCase throws MissingParamError if no password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -5,6 +5,9 @@ class AuthUseCase {
     if (!email) {
       throw new MissingParamError("email");
     }
+    if (!password) {
+      throw new MissingParamError("password");
+    }
 
     // return this.accessToken;
   }
@@ -15,5 +18,10 @@ describe("Auth UseCase", () => {
     const sut = new AuthUseCase();
     const promise = sut.auth();
     expect(promise).rejects.toThrow(new MissingParamError("email"));
+  });
+  test("Should throw if no email is provided", async () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth("any_email@email.com");
+    expect(promise).rejects.toThrow(new MissingParamError("password"));
   });
 });
