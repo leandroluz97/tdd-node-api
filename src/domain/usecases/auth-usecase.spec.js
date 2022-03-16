3830f68654529c8d860f4cd3bb3fc4c5c98daf53
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-16 21:34:07 +0000

    change AuthUseCase constructor to receive  an object

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -48,7 +48,11 @@ const makeSut = () => {
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository();
   const encrypterSpy = makeEncrypter();
   const tokenGeneratorSpy = makeTokenGenerator();
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy);
+  const sut = new AuthUseCase({
+    loadUserByEmailRepository: loadUserByEmailRepositorySpy,
+    encrypter: encrypterSpy,
+    tokenGenerator: tokenGeneratorSpy,
+  });
 
   return {
     sut,
@@ -60,13 +64,13 @@ const makeSut = () => {
 
 describe("Auth UseCase", () => {
   test("Should throw if no email is provided", async () => {
-    const sut = new AuthUseCase();
+    const { sut } = makeSut();
     const promise = sut.auth();
     expect(promise).rejects.toThrow(new MissingParamError("email"));
   });
 
   test("Should throw if no email is provided", async () => {
-    const sut = new AuthUseCase();
+    const { sut } = makeSut();
     const promise = sut.auth("any_email@email.com");
     expect(promise).rejects.toThrow(new MissingParamError("password"));
   });
@@ -78,13 +82,13 @@ describe("Auth UseCase", () => {
   });
 
   test("Should throw if no LoadUserByEmailRepository is provided", async () => {
-    const sut = new AuthUseCase();
+    const sut = new AuthUseCase({});
     const promise = sut.auth("any_email@email.com", "any_password");
     expect(promise).rejects.toThrow();
   });
 
   test("Should throw if no LoadUserByEmailRepository has no load method", async () => {
-    const sut = new AuthUseCase({});
+    const sut = new AuthUseCase({ loadUserByEmailRepository: {} });
     const promise = sut.auth("any_email@email.com", "any_password");
     expect(promise).rejects.toThrow();
   });
