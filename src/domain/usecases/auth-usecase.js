9acc8916c01397cffd30ed09d4ce3f716f523c86
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-16 21:18:18 +0000

    refactored AthUseCase

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -14,26 +14,13 @@ module.exports = class AuthUseCase {
       throw new MissingParamError("password");
     }
 
-    // if (!this.loadUserByEmailRepository) {
-    //   throw new MissingParamError("loadUserByEmailRepository");
-    // }
-    // if (!this.loadUserByEmailRepository.load) {
-    //   throw new InvalidParamError("loadUserByEmailRepository");
-    // }
     const user = await this.loadUserByEmailRepository.load(email);
-    if (!user) {
-      return null;
+    const isValid = user && (await this.encrypter.compare(password, user.password));
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id);
+      return accessToken;
     }
 
-    const isValid = await this.encrypter.compare(password, user.password);
-    if (!isValid) {
-      return null;
-    }
-
-    const accessToken = await this.tokenGenerator.generate(user.id);
-    if (!accessToken) {
-      return null;
-    }
-    return accessToken;
+    return null;
   }
 };
