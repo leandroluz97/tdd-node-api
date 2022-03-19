9614e3756ea6d9577467852eed5b41f12e1276ce
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-19 21:04:52 +0000

    move sut creation to a factory function

--- src/utils/encrypter.spec.js
+++ src/utils/encrypter.spec.js
@@ -7,22 +7,26 @@ class Encrypter {
   }
 }
 
+const makeSut = () => {
+  const enrypterSpy = new Encrypter();
+  return enrypterSpy;
+};
 describe("En crypter", () => {
   test("Should teturn true if bcrypt return true", async () => {
-    const sut = new Encrypter();
+    const sut = makeSut();
     const isValid = await sut.compare("any_value", "hashed_value");
     expect(isValid).toBe(true);
   });
 
   test("Should teturn false if bcrypt return false", async () => {
-    const sut = new Encrypter();
+    const sut = makeSut();
     bcrypt.isValid = false;
     const isValid = await sut.compare("any_value", "hashed_value");
     expect(isValid).toBe(false);
   });
 
   test("Should teturn false if bcrypt return false", async () => {
-    const sut = new Encrypter();
+    const sut = makeSut();
     const isValid = await sut.compare("any_value", "hashed_value");
     expect(bcrypt.value).toBe("any_value");
     expect(bcrypt.hash).toBe("hashed_value");
