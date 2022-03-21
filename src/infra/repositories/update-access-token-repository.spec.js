0de1fb2259513f70b90b1d2417106a6501575691
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-21 08:27:26 +0000

    ensure UpdateAccessTokenRepository updated user with given accessToken

---
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -0,0 +1,49 @@
+const { MissingParamError } = require("../../utils/errors");
+const MongoHelper = require("../helper/mongo-helper");
+let db;
+
+class UpdateAccessTokenRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
+  async update(userId, accessToken) {
+    // if (!email) {
+    //   throw new MissingParamError("email");
+    // }
+    const user = await this.userModel.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          accessToken,
+        },
+      }
+    );
+    return user;
+  }
+}
+
+describe("UpdatedAccessToken Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL, "");
+    db = await MongoHelper.getDb();
+  });
+
+  afterEach(async () => {
+    await db.collection("users").deleteMany();
+  });
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test("Should update user with given accessToken", async () => {
+    const userModel = db.collection("users");
+    const sut = new UpdateAccessTokenRepository(userModel);
+    const fakeUser = await userModel.insertOne({
+      email: "valid_email@email.com",
+      password: "hashed_password",
+    });
+    const user = await sut.update(fakeUser.insertedId, "valid_token");
+    const updatedFakeUser = await userModel.findOne({ _id: fakeUser.insertedId });
+    expect(updatedFakeUser.accessToken).toBe("valid_token");
+  });
+});
