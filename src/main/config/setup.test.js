88954b71cdb48d3514c6be4e9037dd6b77e8a05b
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-21 21:25:30 +0000

    enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -10,4 +10,14 @@ describe("App Setup", () => {
     const result = await request(app).get("/test_x_powered_by");
     expect(result.header["x-powered-by"]).toBeUndefined();
   });
+
+  test("Should enable CORS", async () => {
+    app.get("/test_cors", () => {
+      res.send();
+    });
+    const result = await request(app).get("/test_x_powered_by");
+    expect(result.header["access-control-allow-origin"]).toBe("*");
+    expect(result.header["access-control-allow-methods"]).toBe("*");
+    expect(result.header["access-control-allow-headers"]).toBe("*");
+  });
 });
