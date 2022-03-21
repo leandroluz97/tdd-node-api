8f99ec937366810d0be96fa6a3e9a39c4c295fd6
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-21 21:31:07 +0000

    move cors test to its own file

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -10,14 +10,4 @@ describe("App Setup", () => {
     const result = await request(app).get("/test_x_powered_by");
     expect(result.header["x-powered-by"]).toBeUndefined();
   });
-
-  test("Should enable CORS", async () => {
-    app.get("/test_cors", () => {
-      res.send();
-    });
-    const result = await request(app).get("/test_x_powered_by");
-    expect(result.header["access-control-allow-origin"]).toBe("*");
-    expect(result.header["access-control-allow-methods"]).toBe("*");
-    expect(result.header["access-control-allow-headers"]).toBe("*");
-  });
 });
---
+++ src/main/middlewares/cors.test.js
@@ -0,0 +1,15 @@
+const res = require("express/lib/response");
+const request = require("supertest");
+const app = require("../config/app");
+
+describe("Cors Middleware", () => {
+  test("Should enable CORS", async () => {
+    app.get("/test_cors", () => {
+      res.send();
+    });
+    const result = await request(app).get("/test_x_powered_by");
+    expect(result.header["access-control-allow-origin"]).toBe("*");
+    expect(result.header["access-control-allow-methods"]).toBe("*");
+    expect(result.header["access-control-allow-headers"]).toBe("*");
+  });
+});
