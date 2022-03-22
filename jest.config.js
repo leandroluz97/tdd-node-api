d002f089554c41091c72a88a80ba75c28a49bdd7
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-22 08:17:37 +0000

    added option to exclude test on src/main folder

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
-  collectCoverageFrom: ["**/src/**/*.js"],
+  collectCoverageFrom: ["**/src/**/*.js", "!**/src/main/**"],
   preset: "@shelf/jest-mongodb",
 };
