7128eb5ed8cd418f2c25d2b6665b33c62b7c9ed2
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-23 20:27:35 +0000

    added login route integration test to ensure 401  response when invalid credentials are provided

--- src/main/routes/login-routes.test.js
+++ src/main/routes/login-routes.test.js
@@ -16,14 +16,31 @@ describe("Login Routes", () => {
   afterAll(async () => {
     await MongoHelper.disconnect();
   });
+
   test("Should return 200 when valid credentials are provided", async () => {
     await userModel.insertOne({
       email: "valid_email@email.com",
       password: bcrypt.hashSync("hashed_password", 10),
     });
-    await request(app).post("/api/login").send({
+    await request(app)
+      .post("/api/login")
+      .send({
+        email: "valid_email@email.com",
+        password: "hashed_password",
+      })
+      .expect(200);
+  });
+  test("Should return 401 when invalid credentials are provided", async () => {
+    await userModel.insertOne({
       email: "valid_email@email.com",
-      password: "hashed_password",
+      password: bcrypt.hashSync("hashed_password", 10),
     });
+    await request(app)
+      .post("/api/login")
+      .send({
+        email: "valid_email@email.com",
+        password: "hashed_password",
+      })
+      .expect(401);
   });
 });
