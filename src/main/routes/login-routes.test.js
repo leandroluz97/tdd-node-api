15ae24b9f159656b8e18b9c50642886093c40c69
leandroluz97 <leandrodaluz97@gmail.com>
2022-03-23 20:24:38 +0000

    added login route integration test to ensure 200 response when valid credentials are provided

---
+++ src/main/routes/login-routes.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const mongoHelper = require("../../infra/helper/mongo-helper");
+const app = require("../config/app");
+const bcrypt = require("bcrypt");
+let userModel;
+
+describe("Login Routes", () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL, "");
+    userModel = await MongoHelper.getCollection("users");
+  });
+
+  afterEach(async () => {
+    await userModel.deleteMany();
+  });
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+  test("Should return 200 when valid credentials are provided", async () => {
+    await userModel.insertOne({
+      email: "valid_email@email.com",
+      password: bcrypt.hashSync("hashed_password", 10),
+    });
+    await request(app).post("/api/login").send({
+      email: "valid_email@email.com",
+      password: "hashed_password",
+    });
+  });
+});
